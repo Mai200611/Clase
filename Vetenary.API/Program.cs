3e696040ade914375c15f6bcba3e0e1cb561ce88
Mai200611 <penagarciam061@gmail.com>
2026-03-20 12:15:59 -0500

    Primer commit

--- Vetenary.API/Program.cs
+++ Vetenary.API/Program.cs
@@ -10,8 +10,19 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+
 {
+
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+
+    {
+
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+
+    });
+
 }
 
 app.UseHttpsRedirection();
