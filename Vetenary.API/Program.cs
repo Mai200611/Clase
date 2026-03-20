2d238f7fee68d993b287a6ada049c4e646809053
Mai200611 <penagarciam061@gmail.com>
2026-03-20 13:33:57 -0500

    2 commit

--- Vetenary.API/Program.cs
+++ Vetenary.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Vetenary.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+IServiceCollection Datacontext = builder.Services.AddDbContext<Datacontext>(x => x.UseSqlServer("name=DefaultConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,21 +17,13 @@ if (app.Environment.IsDevelopment())
 {
 
     app.MapOpenApi();
-
     app.UseSwaggerUI(options =>
-
     {
-
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
-
     });
-
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
